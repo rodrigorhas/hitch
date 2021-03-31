e36661c3daeeb93e5f60610973a255b1d55a9036
rodrigorhas <rodrigo@edinheiro.org>
2021-03-31 02:37:22 -0300

    update: randomHash

--- src/js/engine/support/Random.js
+++ src/js/engine/support/Random.js
@@ -1,4 +1,5 @@
-export const randomHash = (size = 7) => Math.random().toString(36).substring(size)
+export const randomHash = (length = 7) => [ ...Array(length) ].map(() => (~~(Math.random() * 36)).toString(36)).join('');
 export const randomHex = () => '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
 export const randomNumber = (min, max) => Math.random() * (max - min) + min;
 export const fastRandomNumber = (min, max) => (Math.random() * (max - min + 1) ) << 0
+
